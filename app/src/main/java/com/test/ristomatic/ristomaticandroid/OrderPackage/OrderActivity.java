e276e38d968e25155ecdd0a2b18e495ab29bda5e
DavideMichelon10 <davidemichelon10@gmail.com>
2018-10-17 14:30:16 +0200

    branch grassi/orderLayout mergiato manualmente

--- app/src/main/java/com/test/ristomatic/ristomaticandroid/OrderPackage/OrderActivity.java
+++ app/src/main/java/com/test/ristomatic/ristomaticandroid/OrderPackage/OrderActivity.java
@@ -4,6 +4,10 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.test.ristomatic.ristomaticandroid.Application.ContextApplication;
 import com.test.ristomatic.ristomaticandroid.MainPackage.MainActivity;
@@ -19,6 +23,25 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
+        RadioGroup rgp = (RadioGroup) findViewById(R.id.flow_group);
+        RadioGroup.LayoutParams rprms;
+
+        for(int i=0;i<5;i++){
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setText(""+(i+1));
+            radioButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    RadioButton thisButton = (RadioButton) v;
+                    System.out.println(thisButton.getText());
+                }
+            });
+            rprms= new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT, RadioGroup.LayoutParams.MATCH_PARENT);
+            rgp.addView(radioButton, -1, rprms);
+            if(i==0) {
+                radioButton.performClick();
+            }
+        }
         orderViewModel = ViewModelProviders.of(this).get(OrderViewModel.class);
         //orderViewModel.init();
 
