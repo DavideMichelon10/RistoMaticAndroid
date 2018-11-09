fdce12218d631dbed0dead62052dc15ed4868024
FunnyStones <alessandro.grassi1998@gmail.com>
2018-11-09 15:54:47 +0100

    Raggruppa codice in onClick in metodi

--- app/src/main/java/com/test/ristomatic/ristomaticandroid/OrderPackage/RecyclerViewAdapter/DishesAdapter.java
+++ app/src/main/java/com/test/ristomatic/ristomaticandroid/OrderPackage/RecyclerViewAdapter/DishesAdapter.java
@@ -65,88 +65,101 @@ public class DishesAdapter extends RecyclerView.Adapter<DishesAdapter.DishViewHo
                             .findViewById(radioButtonIdid))
                             .getText());
                     //Vero se la portata esiste già
-                    boolean courseExistance = false;
                     try{
                         SelectedDish insertedDish = new SelectedDish(dishes.get(getAdapterPosition()).getDishName());
-
-
-                    //Scorre la lista e controlla se esiste già la portata selezionata
-                    for (int i=0;i< CoursesAdapter.getCourses().size();i++){
-                        if(CoursesAdapter.getCourses().get(i).getCourseNumber() == (courseNumber))
-                        {
-                            courseExistance = true;
-                            break;
-                        }
-                    }
                     //Se la portata non esiste ne viene creata una nuova con il numero di portata e viene aggiunta alla lista
                     //successivamente viene chiamato il notifyItemInserted sulla recyclerViewCourses
-                    if(!courseExistance){
-                        //La portata che verrà inserita
-                        Course courseInserted = new Course(courseNumber);
-                        //Aggiungi il piatto da inserire alla portata
-                        courseInserted.addSelectedDish(insertedDish);
-                        //Se indexCoursePosition è uguale a -1 allora basta fare l'add
-                        int indexCoursePosition = isCoursePositionOccupied(courseNumber);
-                        //Se la portata da inserire non va inserita in una posizione già occupata da un altra portata
-                        if( indexCoursePosition == -1){
-                            CoursesAdapter.getCourses().add(courseInserted);
-                            ((RecyclerView)((OrderActivity)context).findViewById(R.id.recyclerViewCourses)).getAdapter().notifyItemInserted(CoursesAdapter.getCourses().size());
-                        }
-                        else {
-                            CoursesAdapter.getCourses().add(indexCoursePosition, courseInserted);
-                            ((RecyclerView)((OrderActivity)context).findViewById(R.id.recyclerViewCourses)).getAdapter().notifyItemInserted(indexCoursePosition);
-                        }
+                    if(!doesCourseExist(courseNumber)){
+                        insertDishInNewCourse(courseNumber, insertedDish);
                     }
 
                     //se la portata esiste
                     else{
-                        final RecyclerView recyclerViewCourses = ((OrderActivity)context)
-                                .findViewById(R.id.recyclerViewCourses);
-                        try {
-                            //ricava la posizione della portata esistente nella lista
-                            for (int i=0;i< CoursesAdapter.getCourses().size();i++){
-                                if(CoursesAdapter.getCourses().get(i).getCourseNumber() == (courseNumber))
-                                {
-                                    coursePosition = i;
-                                    break;
-                                }
-                            }
-                            //Aggiungo insertedDish nella lista di piatti selezionati all'interno della portata
-                            CoursesAdapter.getCourses().get(coursePosition).addSelectedDish(insertedDish);
-                            //Ricavo la recyclerViewCourses
-
-                            //Chiamo il notifyItemInserted sull'adapter della portata e come position passo la grandezza della lista
-                            //di piatti selezionati meno 1
-                            ((CoursesAdapter.CourseViewHolder)recyclerViewCourses
-                                    .findViewHolderForAdapterPosition(coursePosition))
-                                    .getRecyclerViewCourse()
-                                    .getAdapter()
-                                    .notifyItemInserted(CoursesAdapter.getCourses().get(coursePosition).getAllSelectedDishes().size()-1);
-                        }catch (NullPointerException exeption){
-                            final int finalCoursePosition = coursePosition;
-                            recyclerViewCourses.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    if((CoursesAdapter.CourseViewHolder)recyclerViewCourses
-                                            .findViewHolderForAdapterPosition(finalCoursePosition) != null)
-                                    {
-                                        ((CoursesAdapter.CourseViewHolder)recyclerViewCourses
-                                                .findViewHolderForAdapterPosition(finalCoursePosition))
-                                                .getRecyclerViewCourse()
-                                                .getAdapter()
-                                                .notifyItemInserted(CoursesAdapter.getCourses().get(finalCoursePosition).getAllSelectedDishes().size()-1);
-                                    }
-                                }
-                            },16);
-                        }
-
+                        insertDishInExistingCourse(courseNumber, insertedDish);
                     }
                     //eccezione tirata quando cambi categoria e selezioni un piatto ed esso è nullo
                     }catch (ArrayIndexOutOfBoundsException e){ }
                 }
             });
         }
-        //Vero se la portata da inserire va inserita in una posizione già occupata da un altra portata
+
+        //Inserisce il piatto nel caso la portata esista già
+        private void insertDishInExistingCourse(int courseNumber, SelectedDish insertedDish)
+        {
+            int coursePosition = 0;
+            final RecyclerView recyclerViewCourses = ((OrderActivity)context)
+                    .findViewById(R.id.recyclerViewCourses);
+            try {
+                //ricava la posizione della portata esistente nella lista
+                for (int i=0;i< CoursesAdapter.getCourses().size();i++){
+                    if(CoursesAdapter.getCourses().get(i).getCourseNumber() == (courseNumber))
+                    {
+                        coursePosition = i;
+                        break;
+                    }
+                }
+                //Aggiungo insertedDish nella lista di piatti selezionati all'interno della portata
+                CoursesAdapter.getCourses().get(coursePosition).addSelectedDish(insertedDish);
+                //Ricavo la recyclerViewCourses
+
+                //Chiamo il notifyItemInserted sull'adapter della portata e come position passo la grandezza della lista
+                //di piatti selezionati meno 1
+                ((CoursesAdapter.CourseViewHolder)recyclerViewCourses
+                        .findViewHolderForAdapterPosition(coursePosition))
+                        .getRecyclerViewCourse()
+                        .getAdapter()
+                        .notifyItemInserted(CoursesAdapter.getCourses().get(coursePosition).getAllSelectedDishes().size()-1);
+            }catch (NullPointerException exeption){
+                final int finalCoursePosition = coursePosition;
+                recyclerViewCourses.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if((CoursesAdapter.CourseViewHolder)recyclerViewCourses
+                                .findViewHolderForAdapterPosition(finalCoursePosition) != null)
+                        {
+                            ((CoursesAdapter.CourseViewHolder)recyclerViewCourses
+                                    .findViewHolderForAdapterPosition(finalCoursePosition))
+                                    .getRecyclerViewCourse()
+                                    .getAdapter()
+                                    .notifyItemInserted(CoursesAdapter.getCourses().get(finalCoursePosition).getAllSelectedDishes().size()-1);
+                        }
+                    }
+                },16);
+            }
+        }
+
+        //Inserisce il piatto nel caso la portata non esiste
+        private void insertDishInNewCourse(int courseNumber, SelectedDish insertedDish)
+        {
+            //La portata che verrà inserita
+            Course courseInserted = new Course(courseNumber);
+            //Aggiungi il piatto da inserire alla portata
+            courseInserted.addSelectedDish(insertedDish);
+            //Se indexCoursePosition è uguale a -1 allora basta fare l'add
+            int indexCoursePosition = isCoursePositionOccupied(courseNumber);
+            //Se la portata da inserire non va inserita in una posizione già occupata da un altra portata
+            if( indexCoursePosition == -1){
+                CoursesAdapter.getCourses().add(courseInserted);
+                ((RecyclerView)((OrderActivity)context).findViewById(R.id.recyclerViewCourses)).getAdapter().notifyItemInserted(CoursesAdapter.getCourses().size());
+            }
+            else {
+                CoursesAdapter.getCourses().add(indexCoursePosition, courseInserted);
+                ((RecyclerView)((OrderActivity)context).findViewById(R.id.recyclerViewCourses)).getAdapter().notifyItemInserted(indexCoursePosition);
+            }
+        }
+
+        //Controlla se la portata da inserire esiste già
+        private boolean doesCourseExist(int courseNumber)
+        {
+            for (int i=0;i< CoursesAdapter.getCourses().size();i++){
+                if(CoursesAdapter.getCourses().get(i).getCourseNumber() == (courseNumber))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //Ritorna -1 se la portata va aggiunta in fondo e l'indice di dova va inserito se non va in fondo
         private int isCoursePositionOccupied(int courseNumber){
             System.out.println("isCoursePositionOccupied");
             for (int i=0;i< CoursesAdapter.getCourses().size();i++){
