c28cd9dccd82dc8451903ab935fc018b29013aed
GrassiAlessandro <alessandro.grassi@studenti.unitn.it>
2019-04-13 16:12:53 +0200

    Aggingi snackBar

--- app/src/main/java/com/test/ristomatic/ristomaticandroid/OrderPackage/OrderRepository.java
+++ app/src/main/java/com/test/ristomatic/ristomaticandroid/OrderPackage/OrderRepository.java
@@ -1,24 +1,18 @@
 package com.test.ristomatic.ristomaticandroid.OrderPackage;
 
-import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.gson.JsonArray;
 import com.test.ristomatic.ristomaticandroid.Application.ContextApplication;
 import com.test.ristomatic.ristomaticandroid.Application.SingeltonVolley;
 import com.test.ristomatic.ristomaticandroid.Application.VolleyCallApplication;
-import com.test.ristomatic.ristomaticandroid.Application.VolleyCallback;
 import com.test.ristomatic.ristomaticandroid.Application.VolleyCallbackObject;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class OrderRepository {
-    public void sendReport(final JSONObject report, final VolleyCallbackObject volleyCallback){
+    public void sendReport(final JSONObject report, final VolleyCallbackObject volleyCallback) {
         JsonObjectRequest sendRequest = new JsonObjectRequest(Request.Method.POST, VolleyCallApplication.report(), report,
                 new Response.Listener<JSONObject>() {
                     @Override
